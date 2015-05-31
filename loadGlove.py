021eae368ac445af0a9ca963b0c8c7a9c9ea9fef
Shenxiu Liu <shenxiu@stanford.edu>
2015-05-31 00:34:22 -0700

    load glove

---
+++ loadGlove.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cPickle as pickle
+
+
+def load_wordvec(filename, save_filename, save_numpyfile):
+    wordvec_dic = {}
+    wordvec_lst = []
+    with open(filename, 'r') as wordvec_file:
+        for i, line in enumerate(wordvec_file):
+            content = line.split()
+            wordvec_dic[content[0]] = i
+            wordvec_lst.append(np.array(map(float, content[1:]), dtype='float32'))
+            if 0 == i % 10000:
+                print i
+    wordvec_lst = np.array(wordvec_lst)
+    np.save(save_numpyfile, wordvec_lst)
+    with open(save_filename, 'wb') as save_file:
+        pickle.dump(wordvec_dic, save_file)
+    return
+
+
+def load_batch(file_prefix):
+    load_wordvec(file_prefix + '.txt', file_prefix + '.bin', file_prefix + '.npy')
+
+
+if __name__ == '__main__':
+    load_batch('./data/glove.twitter.27B.25d')
+    # g_filename = './data/train.json'
+    # g_data = []
+    # with open(g_filename) as handle:
+    #     for line in handle:
+    #         g_data.append(json.loads(line))
+    #         g_data[-1]['tree'] = nltk.tree.Tree.fromstring(g_data[-1]['tree'])
